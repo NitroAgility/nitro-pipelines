3eccd5bec5453b163080e188b680105b3d143105
Nicola Gallo <nicolagmt@hotmail.com>
2022-05-09 22:09:24 +0200

    Amended logic to install nitro

--- get-nitro.sh
+++ get-nitro.sh
@@ -3,6 +3,6 @@ git clone https://github.com/NitroAgility/nitro-pipelines.git
 pushd ./nitro-pipelines > /dev/null
 go install . && go build -o nitro 
 mv ./nitro ../nitro
-mv ./jenkins/pipe/nitro-pipe ../nitro-pipe
+mv ./jenkins/nitro-pipe ../nitro-pipe
 popd > /dev/null
 chmod +x ./nitro && chmod +x ./nitro-pipe
