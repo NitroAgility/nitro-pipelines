ff44224ca629b2e7d78979c4221a5ae7f4ff67b8
Nicola Gallo <nicolagmt@hotmail.com>
2022-05-10 11:13:30 +0200

    Code refactoring

--- scripts/get-nitro.sh
+++ scripts/get-nitro.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 git clone https://github.com/NitroAgility/nitro-pipelines.git
 pushd ./nitro-pipelines > /dev/null
+git checkout v2.0
 go install . && go build -o nitro 
 mv ./nitro ../nitro
-mv ./jenkins/nitro-pipe ../nitro-pipe
+mv ./scripts/nitro-pipe ../nitro-pipe
 popd > /dev/null
 chmod +x ./nitro && chmod +x ./nitro-pipe
 rm -rf ./nitro-pipelines
