f43ed3cd464de6ac482ef223c68c1981950b9e86
Nicola Gallo <nicolagmt@hotmail.com>
2022-05-13 19:11:38 +0200

    solution refactoring

--- pkg/core/commands/execute_deploy.go
+++ pkg/core/commands/execute_deploy.go
@@ -91,9 +91,9 @@ exit_code=$? && if [ $exit_code -ne 0 ]; then exit $exit_code; fi
 # Post deployment
 {{ .PostDeployment }}
 exit_code=$? && if [ $exit_code -ne 0 ]; then exit $exit_code; fi
-rm "$NITROBIN/aws_credentials"
-rm "$NITROBIN/aws_config"
-rm "$NITROBIN/kube_config"
+#rm "$NITROBIN/aws_credentials"
+#rm "$NITROBIN/aws_config"
+#rm "$NITROBIN/kube_config"
 # Cleaning expanded variables
 {{ range .Expand -}}
 {{ if eq .Type "file" -}}
